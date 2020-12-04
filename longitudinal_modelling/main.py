07f339894daf40a8bc5f6889baf767f8499bbbd9
aurelie <aurelie.mascio@kcl.ac.uk>
2020-12-04 16:54:23 +0000

    added

--- longitudinal_modelling/main.py
+++ longitudinal_modelling/main.py
@@ -1,3 +1,5 @@
+#https://www.statsmodels.org/stable/examples/notebooks/generated/mixed_lm_example.html
+
 from longitudinal_modelling.longitudinal_utils import *
 from longitudinal_modelling.data_stats import *
 import statsmodels.api as sm
@@ -16,4 +18,12 @@ md = smf.mixedlm(r_formula, df, groups=df['brcid'])
 md = smf.mixedlm(r_formula, df, groups=df['brcid'], re_formula="~date")
 
 mdf = md.fit(method=["lbfgs"])
+print(mdf.summary())
+
+# fit a model in which the two random effects are constrained to be uncorrelated:
+md = smf.mixedlm(r_formula, df, groups=df['brcid'], re_formula="~date")
+free = sm.regression.mixed_linear_model.MixedLMParams.from_components(np.ones(2),
+                                                                      np.eye(2))
+
+mdf = md.fit(free=free, method=["lbfgs"])
 print(mdf.summary())
