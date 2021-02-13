02d2bfb808c2257f188fdbf200d502cdd688c251
KCLaurelie <aurelie.mascio@kcl.ac.uk>
2021-02-13 18:30:51 +0000

    minor improvements

--- longitudinal_modelling/main.py
+++ longitudinal_modelling/main.py
@@ -9,19 +9,16 @@ import statsmodels.regression.mixed_linear_model as mlm
 
 mmse_obj = LongitudinalDataset(
     data=r'/Users/aurelie/PycharmProjects/prometheus/longitudinal_modelling/trajectories_synthetic.xlsm'
-    , sheet_name='data', target='score_new', timestamp='date', group='brcid', covariates=['age', 'med', 'gender'])
+    , sheet_name='data', target='score_inv', timestamp='date', group='brcid', covariates=['age', 'med', 'gender'])
 honos_obj = LongitudinalDataset(
     data=r'/Users/aurelie/PycharmProjects/prometheus/longitudinal_modelling/f20_all_classification_by_year_20200209.xlsx'
-    , sheet_name='fake', target='honos_adjusted_total', timestamp='score_year', group='brcid'
-    , covariates=['age_at_score_baseline', 'nlp_sc', 'Cognitive_Problems_Score_ID', 'diagnosis', 'education_level', 'gender'])
-data_obj = LongitudinalDataset(
-    data=r'/Users/aurelie/PycharmProjects/prometheus/longitudinal_modelling/f20_all_classification_by_year_20200209.xlsx'
-    , sheet_name='traj', target='honos_adjusted_total', timestamp='score_year', group='brcid'
+    , sheet_name='traj', target='honos_adjusted_total', timestamp='score_year_centered', group='brcid'
     , covariates=['age_at_score_baseline', 'nlp_sc_anytime', 'Cognitive_Problems_Score_ID_anytime'
+        , 'nlp_sc_baseline', 'Cognitive_Problems_Score_ID_baseline', 'nlp_sc', 'Cognitive_Problems_Score_ID'
         , 'diagnosis', 'education_level', 'gender', 'ethnicity', 'first_language', 'doa', 'marital_status'
         , 'antidementia_medication_baseline', 'antidepressant_medication_baseline', 'antipsychotic_medication_baseline'])
 
-obj = data_obj
+obj = honos_obj
 df = obj.load_data()
 for col in ['antidementia_medication_baseline', 'antidepressant_medication_baseline', 'antipsychotic_medication_baseline']:
     df[col] = np.where(df[col] != 'no', 'yes', 'no')
@@ -31,7 +28,10 @@ for col in ['education_level', 'nlp_sc', 'Cognitive_Problems_Score_ID'
     if col in df.columns: df[col] = np.where(pd.to_numeric(df[col]) > 0, 'yes', 'no')
 
 # r_formula = 'score ~  date + age + diagnosis + gender + date * age + date * diagnosis + date * gender'
-r_formula = make_smf_formula(target=obj.target, covariates=obj.covariates, timestamp=obj.timestamp)
+cov = ['age_at_score_baseline', 'nlp_sc', 'Cognitive_Problems_Score_ID'
+        , 'diagnosis', 'education_level', 'gender', 'ethnicity', 'first_language', 'doa', 'marital_status'
+        , 'antidementia_medication_baseline', 'antidepressant_medication_baseline', 'antipsychotic_medication_baseline']
+r_formula = make_smf_formula(target=obj.target, covariates=cov, timestamp=obj.timestamp)
 
 # random intercept only
 md = smf.mixedlm(r_formula, df, groups=df[obj.group])
