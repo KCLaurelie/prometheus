2cde163e5da31a8c0fea7aa613d815fa27cf248c
KCLaurelie <aurelie.mascio@kcl.ac.uk>
2021-02-27 12:02:29 +0000

    cleanup

--- longitudinal_modelling/main.py
+++ longitudinal_modelling/main.py
@@ -23,10 +23,11 @@ df['patient_discharged'] = np.where(df['num_ward_discharges'] > 0, 1, 0)
 df['ward_len_normalized'] = df['ward_len'] / df['ward_len'].max()
 df['has_dementia'] = np.where(df['diagnosis'] == 'schizo+dementia', 1, 0)
 df['gender_male'] = np.where(df['gender'] == 'male', 1, 0)
-
 for col in ['antidementia_medication_baseline', 'antidepressant_medication_baseline', 'antipsychotic_medication_baseline'
     , 'antidementia_medication', 'antidepressant_medication', 'antipsychotic_medication']:
     df[col] = np.where(df[col].str.lower().isin(['no', np.nan, 'null', '#n/a', 0]), 0, 1)
+for col in ['Cognitive_Problems_Score_ID', 'Daily_Living_Problems_Score_ID', 'Hallucinations_Score_ID', 'Living_Conditions_Problems_Score_ID', 'Relationship_Problems_Score_ID']:
+    df[col+'_bool'] = np.where(df[col] >= 2, 1, 0)
 # df_reg = df.loc[(df.honos_adjusted_total > 0) & (df.honos_adjusted_total <= 30)]
 # df_reg = df.loc[(df.age_at_score >= 18) & (df.age_at_score <= 90)]
 
@@ -40,17 +41,19 @@ res = fit_mlm(df, group=obj.group, target=obj.target, covariates=cov_mlm, timest
 (res.tables[1]).to_clipboard()
 
 # REGRESSION
-cov_reg = ['nlp_num_symptoms', 'Cognitive_Problems_Score_ID', 'Daily_Living_Problems_Score_ID', 'Hallucinations_Score_ID', 'Living_Conditions_Problems_Score_ID', 'Relationship_Problems_Score_ID'
+cov_reg = ['nlp_num_symptoms'
+    , 'Cognitive_Problems_Score_ID_bool', 'Daily_Living_Problems_Score_ID_bool', 'Hallucinations_Score_ID_bool', 'Living_Conditions_Problems_Score_ID_bool', 'Relationship_Problems_Score_ID_bool'
     , 'has_dementia', 'education', 'gender_male', 'ethnicity_white', 'first_language_EN', 'married_or_cohab', 'employed_or_active'
     , 'antidementia_medication', 'antidepressant_medication', 'antipsychotic_medication']
-res_reg = fit_reg(df, target=obj.target, covariates=cov_reg, timestamp='age_at_score', reg_type='ols', dummyfy_non_num=True, intercept=False, round_stats=5)
-res_reg = fit_reg(df, target='ward_len', covariates=cov_reg, timestamp='age_at_score', reg_type='ols', dummyfy_non_num=True, intercept=False, round_stats=5)
-res_reg = fit_reg(df, target='patient_in_ward', covariates=cov_reg, timestamp='age_at_score', reg_type='logit', dummyfy_non_num=True, intercept=False)
-res_reg = fit_reg(df, target='patient_discharged', covariates=cov_reg, timestamp='age_at_score', reg_type='logit', dummyfy_non_num=True, intercept=False)
-res_reg = fit_reg(df, target='nlp_num_symptoms', covariates=cov_reg, timestamp='age_at_score', reg_type='ols', dummyfy_non_num=True, intercept=False)
+res_reg = fit_reg(df, target=obj.target, covariates=cov_reg, timestamp='age_at_score', reg_fn=sm.OLS, dummyfy_non_num=True, intercept=False, round_stats=5)
+res_reg = fit_reg(df, target='ward_len', covariates=cov_reg, timestamp='age_at_score', reg_fn=sm.OLS, dummyfy_non_num=True, intercept=False, round_stats=5)
+res_reg = fit_reg(df, target='patient_in_ward', covariates=cov_reg, timestamp='age_at_score', reg_fn=sm.Logit, dummyfy_non_num=True, intercept=False)
+res_reg = fit_reg(df, target='patient_discharged', covariates=cov_reg, timestamp='age_at_score', reg_fn=sm.Logit, dummyfy_non_num=True, intercept=False)
+res_reg = fit_reg(df, target='nlp_num_symptoms', covariates=cov_reg, timestamp='age_at_score', reg_fn=sm.OLS, dummyfy_non_num=True, intercept=False)
 
 
 res_reg['model'].summary2().tables[0].append(res_reg['model'].summary2().tables[2]).to_clipboard(index=False, header=False)  # regression goodness of fit stats
+res_reg['model'].summary2().tables[0].to_clipboard(index=False, header=False)  # regression goodness of fit stats
 res_reg['model'].summary2().tables[1].to_clipboard()  # coeffs
 
 ## MANUAL ANALYSIS
@@ -59,13 +62,8 @@ r_formula = make_smf_formula(target=obj.target, covariates=cov_mlm, timestamp=ob
 md = smf.mixedlm(r_formula, df, groups=df[obj.group], re_formula='~'+obj.timestamp)
 mdf = md.fit(method=['lbfgs'], reml=True)
 
-
 X = df[cov_reg + ['age_at_score']]
-model = sm.OLS(df['ward_len'], df[['age_at_score', 'nlp_num_symptoms']]).fit()  # length of admission
-model = sm.OLS(df['ward_len'], df[cov_reg].fillna(0)).fit()  # length of admission
-model = sm.Logit(df['patient_in_ward'], df[['age_at_score', 'nlp_num_symptoms']]).fit()  # admission
 model = sm.Logit(1 * (df['num_ward_entries'] > 1), df[['age_at_score', 'nlp_num_symptoms']]).fit()  # readmission
-
 model = sm.GLM(np.exp(df['ward_len_normalized']), sm.add_constant(X), family=sm.families.Gamma(link=sm.families.links.log())).fit()
 print(model.summary())
 model.summary2().tables[0].append(model.summary2().tables[2]).to_clipboard(index=False, header=False)
--- longitudinal_modelling/regression.py
+++ longitudinal_modelling/regression.py
@@ -1,11 +1,7 @@
 from sklearn.model_selection import train_test_split
 from longitudinal_modelling.longitudinal_utils import *
-from sklearn.linear_model import LinearRegression, LogisticRegression
-import sklearn.metrics as metrics
 import statsmodels.api as sm
-import statsmodels.miscmodels as mm
 from sklearn import metrics
-from mord import LogisticAT
 
 # logistic regression: nlp_sc_anytime versus covariates
 # ordinal regression: honos score_baseline (cog + total) versus covariates
@@ -15,12 +11,13 @@ def round_down(num, divisor):
     return num - (num % divisor)
 
 
-def fit_reg(df, target, covariates, timestamp
-            , reg_type=LogisticRegression(solver='sag', max_iter=1000)
+def fit_reg(df, target, covariates, timestamp=None
+            , reg_fn=sm.OLS
             , intercept=True
             , test_size=0, random_state=911, round_stats=None
             , dummyfy_non_num=False,  cols_to_dummyfy=None):
     df_local = df.copy()
+    covariates, target, timestamp = [to_list(covariates), to_list(target), to_list(timestamp)]
     # GENERATE DUMMY VARIABLES
     if dummyfy_non_num:
         if cols_to_dummyfy is None:
@@ -32,23 +29,13 @@ def fit_reg(df, target, covariates, timestamp
             df_local = pd.concat([df_local, dummyfied_cols], axis=1, sort=True)
     # SPLIT TEST/TRAINING SETS
     if test_size > 0:
-        x_train, x_test, y_train, y_test = train_test_split(df_local[list(covariates) + [timestamp]], df_local[target], test_size=test_size, random_state=random_state)
+        x_train, x_test, y_train, y_test = train_test_split(df_local[covariates + timestamp], df_local[target], test_size=test_size, random_state=random_state)
     else:
-        x_train, y_train = [df_local[list(covariates) + [timestamp]], df_local[target]]
+        x_train, y_train = [df_local[covariates + timestamp], df_local[target]]
     # FIT REGRESSION
     if intercept: x_train = sm.add_constant(x_train)
-    if reg_type == 'ols':  # linear regression from statsmodel
-        reg_type = sm.OLS(y_train, x_train).fit()
-    elif reg_type == 'logit':  # logistic regression from statsmodel
-        reg_type = sm.Logit(y_train, x_train).fit()
-    elif reg_type == 'ordinal':  # ordinal regression, only available in mord at the moment
-        if x_train.ndim == 1:  # need to reshape if only 1 feature
-            x_train = np.array(x_train).reshape(-1, 1)
-        reg_type = LogisticAT(alpha=0).fit(x_train, y_train)
-    else:  # will work to call any model from sklearn
-        reg_type.fit(x_train, y_train)
-
-    preds_train = reg_type.predict(x_train)
+    fitted_reg = reg_fn(y_train, x_train).fit()
+    preds_train = fitted_reg.predict(x_train)
 
     # GENERATE STATS REPORT
     if round_stats is not None:
@@ -58,7 +45,7 @@ def fit_reg(df, target, covariates, timestamp
     print('training scores:\n', report)
 
     del df_local
-    return {'model': reg_type, 'y_pred': preds_train, 'y_true': y_train, 'report': report}
+    return {'model': fitted_reg, 'y_pred': preds_train, 'y_true': y_train, 'report': report}
 
 
 def regression_results(y_true, y_pred):
