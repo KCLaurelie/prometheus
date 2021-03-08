753de30a9cbc056788b3f768ca74d30afffa05b0
KCLaurelie <aurelie.mascio@kcl.ac.uk>
2021-03-08 17:36:56 +0000

    minor additions

--- longitudinal_modelling/main.py
+++ longitudinal_modelling/main.py
@@ -6,22 +6,28 @@ from longitudinal_modelling.regression import *
 mmse_obj = LongitudinalDataset(
     data=r'/Users/aurelie/PycharmProjects/prometheus/longitudinal_modelling/trajectories_synthetic.xlsm'
     , sheet_name='data', target='score_inv', timestamp='date', group='brcid', covariates=['age', 'med', 'gender'])
+nlp_ci = LongitudinalDataset(
+    data=r'/Users/aurelie/PycharmProjects/prometheus/longitudinal_modelling/nlp_ci_traj_20200303.xlsx'
+    , sheet_name='traj', target='nlp_num_symptoms', timestamp='score_year_centered', group='brcid')
 honos_obj = LongitudinalDataset(
     data=r'/Users/aurelie/PycharmProjects/prometheus/longitudinal_modelling/f20_all_classification_by_year_20210209.xlsx'
     , sheet_name='honos', target='honos_adjusted_total', timestamp='score_year_centered', group='brcid')
+obj = nlp_ci
 obj = honos_obj
 df = obj.load_data()
 
 # DERIVED FIELDS
 df['ethnicity_white'] = np.where(df['ethnicity'] == 'white', 1, 0)
-df['first_language_EN'] = np.where(df['first_language'] == 'not english', 0, 1)
+df['first_language_EN'] = np.where(df['first_language'] == 'english', 1, 0)
 df['married_or_cohab'] = np.where(df['marital_status'] == 'married_cohabitating', 1, 0)
 df['employed_or_active'] = np.where(df['employment'].isin(['employed', 'retired_or_at_home', 'student_or_volunteer']), 1, 0)
 df['no_dementia'] = np.where(df['diagnosis'] == 'schizo+dementia', 0, 1)
 df['gender_male'] = np.where(df['gender'] == 'male', 1, 0)
 df['gender_female'] = np.where(df['gender'] == 'male', 0, 1)
+df['age_at_score'] = df['age_at_score'].clip(lower=10)
 df['readmission'] = 1 * (df['num_ward_entries'] > 1)
 df['ward_len_normalized'] = df['ward_len'] / df['ward_len'].max()
+df['nlp_sc_bool'] = np.where(df['nlp_num_symptoms'] >= 1, 1, 0)
 cov_sociodem = ['gender_male', 'no_dementia', 'education', 'ethnicity_white', 'first_language_EN', 'married_or_cohab', 'employed_or_active']
 cov_meds = ['antidementia_medication', 'antidepressant_medication', 'antipsychotic_medication']
 cov_scores = ['Cognitive_Problems_Score_ID', 'Daily_Living_Problems_Score_ID', 'Hallucinations_Score_ID', 'Living_Conditions_Problems_Score_ID', 'Relationship_Problems_Score_ID'
@@ -30,17 +36,19 @@ cov_scores = ['Cognitive_Problems_Score_ID', 'Daily_Living_Problems_Score_ID', '
 for col in cov_meds:
     df[col + '_bool'] = np.where(df[col].str.lower().isin(['no', np.nan, 'null', '#n/a', 0]), 0, 1)
 for col in cov_scores:
-    df[col + '_bool'] = np.where(df[col] >= 1, 1, 0) #if (('nlp' in col) or ('ward' in col)) else np.where(df[col] >= 2, 1, 0)
+    if col in df.columns: df[col + '_bool'] = np.where(df[col] >= 1, 1, 0)
 # df_reg = df.loc[(df.honos_adjusted_total > 0) & (df.honos_adjusted_total <= 30)]
 # df_reg = df.loc[(df.age_at_score >= 18) & (df.age_at_score <= 90)]
 
 # #####################################
 # TRAJECTORIES MODELLING
-df_baseline = df.loc[df.baseline == 1][[obj.group] + [col + '_bool' for col in cov_meds + cov_scores]]
+baseline_cols = [x for x in df.columns if x in ['age_at_score'] + [col + '_bool' for col in cov_meds] + [col + '_bool' for col in cov_scores]]
+df_baseline = df.loc[df.score_year_centered == 1][[obj.group] + baseline_cols]
 df = df.join(df_baseline.set_index(obj.group), on=obj.group, rsuffix='_baseline')
 cov_honos_baseline = [col for col in df.columns if 'Score_ID_bool_baseline' in col]
 cov_meds_baseline = [col + '_bool_baseline' for col in cov_meds]
 res = fit_mlm(df, group=obj.group, target=obj.target, covariates=['nlp_sc_bool_baseline']+cov_sociodem+cov_meds_baseline, timestamp=obj.timestamp, rdn_slope=True, method=['lbfgs'])
+res = fit_mlm(df, group=obj.group, target=obj.target, covariates=['age_at_score']+cov_sociodem+cov_meds_baseline, timestamp=obj.timestamp, rdn_slope=True, method=['lbfgs'])
 (res['stats']).to_clipboard(index=False, header=False)
 (res['coeffs']).to_clipboard()
 
@@ -61,6 +69,9 @@ res_reg['coeffs'].to_clipboard()  # coeffs
 # #####################################
 # MANUAL ANALYSIS
 # r_formula = 'score ~  date + age + diagnosis + gender + date * age + date * diagnosis + date * gender'
+df = pd.read_excel(r'/Users/aurelie/PycharmProjects/prometheus/longitudinal_modelling/nlp_ci_traj_20200303.xlsx', sheet_name='traj', engine='openpyxl')
+res = fit_mlm(df, group='brcid', target='num_symptoms', covariates=cov_sociodem, timestamp=obj.timestamp, rdn_slope=True, method=['lbfgs'])
+
 r_formula = make_smf_formula(target=obj.target, covariates=cov_reg, timestamp=obj.timestamp)
 md = smf.mixedlm(r_formula, df, groups=df[obj.group], re_formula='~'+obj.timestamp)
 mdf = md.fit(method=['lbfgs'], reml=True)
