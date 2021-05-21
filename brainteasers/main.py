b5c21ca1146eeb836900507e842efdb8e349efdb
KCLaurelie <aurelie.mascio@kcl.ac.uk>
2021-05-21 15:25:12 +0100

    +1

--- brainteasers/main.py
+++ brainteasers/main.py
@@ -1,8 +1,12 @@
+# https://www.educative.io/blog/crack-amazon-coding-interview-questions#overview
 import timeit
+import collections
 
 """
 1. find missing number in array
 You are given an array of positive numbers from 1 to n, such that all numbers from 1 to n are present except one number x. You have to find x.
+Runtime Complexity: Linear, O(n)
+Memory Complexity: Constant, O(1)
 """
 
 
@@ -24,6 +28,8 @@ print("The time difference is :", timeit.default_timer() - starttime)
 2. Determine if the sum of two integers is equal to the given value
 Given an array of integers and a value, determine if there are any two integers in the array whose sum is equal to the given value. 
 Return true if the sum exists and return false if it does not.
+Runtime Complexity: Linear, O(n)
+Memory Complexity: Linear, O(n)
 """
 
 def find_sum_of_two(A, val):
@@ -44,8 +50,12 @@ Given two sorted linked lists, merge them so that the resulting linked list is a
 Consider two sorted linked lists and the merged list below them as an example.
 https://realpython.com/linked-lists-python/
 https://dbader.org/blog/python-linked-list
+
+Runtime Complexity: Linear, O(m + n) where m and n are lengths of both linked lists
+Memory Complexity: Constant, O(1)
 """
-import collections
+
+# METHOD 2: using deque
 llst = collections.deque()  # create linked list
 llst1 = collections.deque(['A', 'C', 'M', 'X'])
 llst2 = collections.deque(['B', 'D', 'E', 'Y'])
@@ -56,7 +66,7 @@ llst.pop()  # remove last element
 def merge_sorted_llst(llst1, llst2):
     merged_llst = collections.deque()
     while llst1:
-        if llst2: # there's still stuff in llst2
+        if llst2: # if there's still stuff in llst2
             el1 = llst1[0]
             el2 = llst2[0]
             if el1 <= el2:
@@ -71,3 +81,70 @@ def merge_sorted_llst(llst1, llst2):
     if llst2: merged_llst += llst2
     return merged_llst
 
+merge_sorted_llst(collections.deque(['A', 'C', 'M', 'X']), collections.deque(['A', 'B', 'D', 'E', 'Y', 'Z']))
+merge_sorted_llst(collections.deque(['A', 'B', 'C', 'D','E','F','G','H','I', 'M', 'X']), collections.deque(['A', 'B', 'D', 'E', 'Y', 'Z']))
+
+
+# METHOD 2: building linked lists objects frm scratch
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+
+class LinkedList:
+    def __init__(self, nodes=None):
+        self.head = None
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(data=elem)
+                node = node.next
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+    def add_first(self, node):  # inserting at the beginning
+        node.next = self.head
+        self.head = node
+
+    def add_last(self, node):  # inserting at the end
+        if self.head is None:
+            self.head = node
+            return
+        for current_node in self:  # traverse the whole list until you reach the end
+            pass
+        current_node.next = node
+
+    def pop_left(self):  # removing first node
+        if self.head is None:
+            raise Exception("List is empty")
+        else:
+            self.head = self.head.next
+            return
+
+    def pop_right(self):  # removing last node
+        if self.head is None:
+            raise Exception("List is empty")
+        else:
+            current_node = self.head
+            while current_node.next:
+                prev_node = current_node
+                current_node = current_node.next
+            prev_node.next = None
+            return
+
+llist = LinkedList(["a", "b", "c", "d", "e"])
+
+
+print(llist, llist.head, llist.pop_left())
