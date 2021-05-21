482eb5976b89cd882b6a932649bfd103c0cadc54
KCLaurelie <aurelie.mascio@kcl.ac.uk>
2021-05-21 16:44:30 +0100

    +1

--- brainteasers/main.py
+++ brainteasers/main.py
@@ -1,10 +1,14 @@
 # https://www.educative.io/blog/crack-amazon-coding-interview-questions#overview
 import timeit
 import collections
+import sys
 
+#region 1. find missing number in array
 """
 1. find missing number in array
-You are given an array of positive numbers from 1 to n, such that all numbers from 1 to n are present except one number x. You have to find x.
+You are given an array of positive numbers from 1 to n, such that all numbers from 1 to n are present except one number x. Y
+ou have to find x.
+
 Runtime Complexity: Linear, O(n)
 Memory Complexity: Constant, O(1)
 """
@@ -23,11 +27,14 @@ def find_missing(input):
 starttime = timeit.default_timer()
 print(find_missing([3,7,1,2,8,4,5]))
 print("The time difference is :", timeit.default_timer() - starttime)
+#endregion
 
+#region 2. Determine if the sum of two integers is equal to the given value
 """
 2. Determine if the sum of two integers is equal to the given value
 Given an array of integers and a value, determine if there are any two integers in the array whose sum is equal to the given value. 
 Return true if the sum exists and return false if it does not.
+
 Runtime Complexity: Linear, O(n)
 Memory Complexity: Linear, O(n)
 """
@@ -43,8 +50,9 @@ def find_sum_of_two(A, val):
     return False
 
 print(find_sum_of_two([3,7,1,2,8,4,5],10))
+#endregion
 
-
+#region 3. Merge two sorted linked lists
 """3. Merge two sorted linked lists
 Given two sorted linked lists, merge them so that the resulting linked list is also sorted. 
 Consider two sorted linked lists and the merged list below them as an example.
@@ -148,3 +156,107 @@ llist = LinkedList(["a", "b", "c", "d", "e"])
 
 
 print(llist, llist.head, llist.pop_left())
+
+#endregion
+
+#region 4. Copy linked list with arbitrary pointer (TODO)
+"""
+4. Copy linked list with arbitrary pointer
+You are given a linked list where the node has two pointers. 
+The first is the regular next pointer. 
+The second pointer is called arbitrary_pointer and it can point to any node in the linked list. 
+Your job is to write code to make a deep copy of the given linked list. 
+Here, deep copy means that any operations on the original list should not affect the copied list.
+
+Runtime Complexity: Linear, O(n)
+Memory Complexity: Linear, O(n)
+"""
+
+# TODO
+
+#endregion
+
+#region 5. Level order Binary Tree Traversal in python
+"""
+5. Level order Binary Tree Traversal in python
+
+https://stephanosterburg.gitbook.io/scrapbook/coding/python/breadth-first-search/level-order-tree-traversal
+Runtime Complexity: Linear, O(n) where n is number of nodes in the binary tree
+Memory Complexity: Linear, O(n)
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+
+        if root is None: return  # Base Case
+        queue = [root]  # Create an empty queue for level order traversal
+
+        while len(queue) > 0:
+            # Print front of queue and remove it from queue
+            print(queue[0].val)
+            node = queue.pop(0)
+
+            # Enqueue left child
+            if node.left is not None: queue.append(node.left)
+            # Enqueue right child
+            if node.right is not None: queue.append(node.right)
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+Solution().levelOrder(root)
+
+#endregion
+
+
+#region 6. Determine if a binary tree is a binary search tree
+"""
+6. Determine if a binary tree is a binary search tree
+Given a Binary Tree, figure out whether it’s a Binary Search Tree. 
+In a binary search tree, each node’s key value is smaller than the key value of all nodes in the right subtree, 
+and is greater than the key values of all nodes in the left subtree
+
+        100
+    50          20
+25      75  125     350
+
+he optimal approach is a regular in-order traversal and in each recursive call, 
+pass maximum and minimum bounds to check whether the current node’s value is within the given bounds.
+Runtime Complexity: Linear, O(n) where n is number of nodes in the binary tree
+Memory Complexity: Linear, O(n)
+"""
+root = TreeNode(100)
+root.left = TreeNode(50)
+root.right = TreeNode(20)
+root.left.left = TreeNode(25)
+root.left.right = TreeNode(75)
+root.right.left = TreeNode(125)
+root.right.right = TreeNode(350)
+
+
+def is_bst_rec(root, min_value, max_value):
+    if root is None: return True
+    if root.data < min_value or root.data > max_value: return False
+
+    return is_bst_rec(root.left, min_value, root.data) and is_bst_rec(root.right, root.data, max_value)
+
+
+def is_bst(root):
+    return is_bst_rec(root, -sys.maxsize - 1, sys.maxsize)
+
+#endregion
