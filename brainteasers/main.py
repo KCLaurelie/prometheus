90bdcddd23a410fb3e951572cbb6f77cdd73db87
KCLaurelie <aurelie.mascio@kcl.ac.uk>
2021-05-28 12:07:06 +0100

    +1

--- brainteasers/main.py
+++ brainteasers/main.py
@@ -1452,7 +1452,7 @@ substrCount('mnonopoo')
 #region Luck balance
 contests = [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]]
 contests = [[13, 1],[10, 1],[9, 1],[8, 1],[13 ,1],[12, 1],[18, 1],[13, 1]]
-nb_lost_ones = 5
+nb_lost_ones = k = 5
 def LuckBalance(contests, nb_lost_ones):
     zeros = sum([el[0] for el in contests if el[1]==0])
     ones = [el for el in contests if el[1]==1]
@@ -1460,4 +1460,24 @@ def LuckBalance(contests, nb_lost_ones):
     ones_won = sum([el[0] for el in ones[nb_lost_ones:][:]])
     ones_lost = sum([el[0] for el in ones[:nb_lost_ones][:]])
     return -zeros-ones_won+ones_lost
+
+def luckBalance(k, contests):
+    balance = 0
+    ls_important = []
+    for i in range(n):
+        li,ti = contests[i]
+        if ti == 0:
+            balance += li
+        else:
+            ls_important.append(li)
+    ls_important.sort()
+    if len(ls_important)>k:
+        for i in range(len(ls_important)-k):
+            balance -= ls_important[i]
+        for i in range(len(ls_important)-k, len(ls_important)):
+            balance += ls_important[i]
+    elif len(ls_important)<=k:
+        for i in range(len(ls_important)):
+            balance += ls_important[i]
+    return balance
 #endregion
